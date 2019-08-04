8ee79532d8d5a03b0322635de5c50edeb341f536
Dana Andres <andres.dana.maria@gmail.com>
2019-08-04 22:08:11 +0200

    Shooping list: clear list, delete item, add new item, etc.

--- shoppingList/script.js
+++ shoppingList/script.js
@@ -1,17 +1,34 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+const ul = document.querySelector("ul");
+const li = document.getElementsByTagName("li");
+const deleteButton = document.getElementsByTagName("i");
+var clearButton = document.getElementById("clear");
+
 
 function inputLength() {
 	return input.value.length;
 }
 
 function createListElement() {
-	var li = document.createElement("li");
-		li.appendChild(document.createTextNode(input.value));
-		ul.appendChild(li);
-		input.value = "";	
-	}
+    var divClassWrapper = document.createElement("div");
+    divClassWrapper.classList.add("li-wrapper");
+
+    var li = document.createElement("li");
+    var createDiv = document.createElement("div");
+
+    li.appendChild(document.createTextNode(input.value));
+    divClassWrapper.appendChild(li);
+    divClassWrapper.appendChild(createDiv);
+    ul.appendChild(divClassWrapper);
+
+    input.value = "";
+    createDiv.classList.add("div");
+    var createDeleteButton = document.createElement("i");
+    createDeleteButton.classList.add("fa", "fa-trash");
+    createDiv.appendChild(createDeleteButton);
+    deleteParentNodeOnClick();
+}
 
 function addListAfterClick() {
 	 if (inputLength() > 0) {
@@ -25,6 +42,48 @@ function addListAfterKeypress(event) {
 	}
 }
 
+function toggleClassDoneOnAndOff(event) {
+    if (event.target.tagName === "LI") {
+        event.target.classList.toggle("done");
+    }
+}
+
+function createDeleteButtonIcon() {
+    for (var ind = 0; ind < li.length; ind++) {
+        var createDeleteButton = document.createElement("i");
+        var createDiv = document.getElementsByClassName("div");
+        console.log(createDiv);
+        createDeleteButton.classList.add("fa", "fa-trash");
+        createDiv[ind].appendChild(createDeleteButton);
+    }
+}
+
+function deleteNodeOnClick(e) {
+    var trash = document.querySelectorAll("i");
+    for (var ind = 0; ind < trash.length; ind++) {
+        console.log(e);
+        this.parentNode.parentNode.remove();
+    }
+}
+
+function deleteParentNodeOnClick() {
+    for (var i = 0; i < deleteButton.length; i++) {
+        deleteButton[i].addEventListener("click", deleteNodeOnClick);
+    }
+}
+
+function clearList() {
+    ul.innerHTML = "";
+}
+
+clearButton.addEventListener("click", clearList);
+
+ul.addEventListener("click", toggleClassDoneOnAndOff);
+
 button.addEventListener("click", addListAfterClick);
 
-input.addEventListener("keypress", addListAfterKeypress)
+input.addEventListener("keypress", addListAfterKeypress);
+
+createDeleteButtonIcon();
+
+deleteParentNodeOnClick();
